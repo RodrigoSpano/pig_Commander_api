969215c77fa80c3ab8fe6bb1016d603e0f106aad
Lucas Correa <correalucas1905@gmail.com>
2023-08-03 16:56:00 -0300

    feature(api/doc/middlewares): add docs and middlewares

--- src/controllers/savings/postSaving.js
+++ src/controllers/savings/postSaving.js
@@ -1,15 +1,26 @@
-const { saving } = require('../../db');
+const { saving, user } = require('../../db');
 
 const postSaving = async (req, res) => {
   try {
     // * I receive the property by body
     const { name, mount, goal } = req.body;
 
+    // * Id of the user
+    const { id } = req.user.dataValues;
+    
+    // * Check if the user exists before creating the saving
+    const userFind = await user.findByPk(id);
+    if (!userFind) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+
     // * Saving are created
     const newSaving = await saving.create({
       name: name.toLowerCase(),
       mount,
       goal,
+      user_id: id,
     });
 
     // * Response that is sent if everything goes ok
