162b4f575f36c2c99479f6f07277db431ef8aa49
Benjamin Padilla <benja.padilla@outlook.com.ar>
2023-08-03 12:47:40 -0300

    fix(api/expenses): Completing all expenses controllers and middlewares

--- src/utils/middlewares/incomesMiddleware.js
+++ src/utils/middlewares/incomesMiddleware.js
@@ -1,17 +1,16 @@
 const { incomes } = require('../../db');
 
-async function incomeExist (req,res,next) {
-    
-        const { idIncome } = req.params;
-        const incomeToUpdate = await incomes.findByPk(idIncome);
+async function incomeExist(req, res, next) {
+  const { idIncome } = req.params;
+  const incomeToUpdate = await incomes.findByPk(idIncome);
 
-        if (!incomeToUpdate) {
-            return res.status(404).json({ message: 'Income not found' });
-        }
+  if (!incomeToUpdate) {
+    return res.status(404).json({ message: 'Income not found' });
+  }
 
-        return next();
+  return next();
 }
 
 module.exports = {
-    incomeExist
-}
+  incomeExist,
+};
