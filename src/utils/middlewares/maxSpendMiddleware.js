1f33232d7a8e148b73c1f6edddc5273a79bd3ca2
Benjamin Padilla <benja.padilla@outlook.com.ar>
2023-08-04 12:08:50 -0300

    fix(api/maxSpend): bugs in maxSpend

--- src/utils/middlewares/maxSpendMiddleware.js
+++ src/utils/middlewares/maxSpendMiddleware.js
@@ -2,36 +2,52 @@ const { maxSpend } = require('../../db');
 
 const postMaxSpendMiddleware = async (req, res, next) => {
   const { mount } = req.body;
-  if (mount) return res.status(400).json({ error: 'All fields are required' });
-  if (mount < 1)
+  const { id: user_id } = req.user.dataValues;
+
+  if (!mount) return res.status(400).json({ error: 'All fields are required' });
+  if (mount < 1) {
     return res.status(400).json({ error: 'mount cannot be less than 1' });
+  }
+  const existingMaxSpend = await maxSpend.findOne({
+    where: { user_id },
+  });
+  if (existingMaxSpend) {
+    return res.status(400).json({ error: 'User already has a max Spend' });
+  }
   return next();
 };
 
 const getMaxSpendMiddleware = async (req, res, next) => {
   const { id: user_id } = req.user.dataValues;
-  const allExepenses = await maxSpend.findAll({
+  const maxSpendsUser = await maxSpend.findOne({
     where: { user_id },
   });
-  if (allExepenses.length === 0) {
-    res.status(404).json({ error: 'Expenses not found' });
+  if (!maxSpendsUser) {
+    res.status(404).json({ error: 'Max Spend not found' });
   }
   return next();
 };
 const deleteMaxSpendMiddleware = async (req, res, next) => {
-  const { id } = req.params;
-  if (!id) return res.status(400).json({ error: 'Id not recieved' });
+  const { id } = req.user.dataValues;
+  const foundedMaxSpend = await maxSpend.findOne({
+    where: { user_id: id },
+  });
+  if (!foundedMaxSpend)
+    return res.status(404).json({ error: 'Max Spend not founded' });
   return next();
 };
 
 const updateMaxSpendMiddleware = async (req, res, next) => {
   const { mount } = req.body;
-  const { id } = req.params;
+  const { id } = req.user.dataValues;
   if (!mount) return res.status(400).json({ error: 'Mount field is required' });
   if (mount < 1)
     return res.status(400).json({ error: 'mount cannot be less than 1' });
-  const existing = await maxSpend.findByPk(id);
-  if (!existing) return res.status(400).json({ error: 'Expense not found' });
+
+  const existing = await maxSpend.findOne({
+    where: { user_id: id },
+  });
+  if (!existing) return res.status(400).json({ error: 'Max Spend not found' });
   return next();
 };
 
