b9b7970508dcfd88ee9cf954bd1c78f4f6782a3e
RodrigoSpano <rorrospano17@gmail.com>
2023-08-08 16:45:11 -0300

    chore(api/categories): optimization of getAll controller

--- src/controllers/category/getAllCategories.js
+++ src/controllers/category/getAllCategories.js
@@ -1,9 +1,11 @@
-const { categories } = require('../../db');
+const getCategoriesHandler = require('../../handlers/categories/defaultCategories');
 
 const getAllCategories = async (req, res) => {
   try {
-    const allCategories = await categories.findAll();
-
+    const allCategories = await getCategoriesHandler();
+    if (!allCategories.length) {
+      return res.status(404).json({ error: 'categories not found' });
+    }
     return res.status(200).json(allCategories);
   } catch (error) {
     return res.status(404).json({ error: error.message });
