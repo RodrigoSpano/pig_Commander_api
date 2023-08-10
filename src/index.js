2464ba3cfc4f4e88fc582431b64d7b8b7da7077c
Benjamin Padilla <benja.padilla@outlook.com.ar>
2023-08-10 11:37:30 -0300

    feture(api/multer): setting multer

--- src/index.js
+++ src/index.js
@@ -14,14 +14,16 @@ const GoogleStrategy = require('./services/passport/passportGoogle');
 
 const app = express();
 
-app.use(session({
-  secret: `${process.env.SESSION_SECRET}`,
-  resave: true,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: `${process.env.SESSION_SECRET}`,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 app.use(morgan('dev'));
 app.use(helmet());
 
