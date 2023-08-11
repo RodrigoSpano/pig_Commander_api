9b758bff89e227c47acd00f50bd4dbdc56da6c1e
RodrigoSpano <rorrospano17@gmail.com>
2023-08-11 18:28:37 -0300

    fix(incomes/error): fix empty array error

--- src/controllers/expenses/getAllExpenses.js
+++ src/controllers/expenses/getAllExpenses.js
@@ -5,7 +5,6 @@ const getAllExpenses = async (req, res) => {
   try {
     const { id } = getTokenPayload(req.headers['authorization']);
     const allExepenses = await expenses.findAll({ where: { user_id: id, automatized: false } });
-    if (!allExepenses.length) return res.status(404).json({ message: 'expenses is empty' });
     return res.status(200).json(allExepenses);
   } catch (error) {
     return res.status(500).json({ error: error.message });
