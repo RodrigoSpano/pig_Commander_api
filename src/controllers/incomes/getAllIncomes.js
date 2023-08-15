bd191309efcd97c2b37bf2f4cf176e6cb3cfce96
David <cordoba_d@rocketmail.com>
2023-08-15 09:49:49 -0300

    fix(props): done

--- src/controllers/incomes/getAllIncomes.js
+++ src/controllers/incomes/getAllIncomes.js
@@ -4,7 +4,7 @@ const { getTokenPayload } = require('../../utils/helpers/authHelpers');
 const getAllIncomes = async (req, res) => {
   try {
     const { id: user_id } = getTokenPayload(req.headers['authorization']);
-    const allIncomes = await incomes.findAll({ where: { user_id, automatized: false } });
+    const allIncomes = await incomes.findAll({ where: { user_id } });
     return res.status(200).json(allIncomes);
   } catch (error) {
     return res.status(500).json({ error: error.message });
