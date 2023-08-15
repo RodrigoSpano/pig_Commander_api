eeb79e84d5e87a5d48f9463ec43bce147f2273df
RodrigoSpano <rorrospano17@gmail.com>
2023-08-15 13:45:38 -0300

    feature(transactions/automatize): create, get and stop automatize transactions

--- src/handlers/automatization/automatizationHandler.js
+++ src/handlers/automatization/automatizationHandler.js
@@ -1,7 +1,9 @@
 /* eslint-disable consistent-return */
 const cron = require('node-cron');
 const { expenses, incomes } = require('../../db');
-const { sendExpensesAutoNotification, sendIncomesAutoNotification } = require('../../utils/helpers/sendMailHelper');
+const {
+  sendExpensesAutoNotification, sendIncomesAutoNotification, sendExpensesNotification, sendIncomesNotification
+} = require('../../utils/helpers/sendMailHelper');
 
 const createExpenseAutomatization = async (data, user_id) => {
   const expenseObj = {
@@ -14,6 +16,7 @@ const createExpenseAutomatization = async (data, user_id) => {
 
   cron.schedule(`0 7 ${data.auto_date} * *`, async () => {
     await expenses.create(expenseObj);
+    sendExpensesNotification(user_id, data.amount, data.name);
   }, { jobName: data.name, user_id });
   await sendExpensesAutoNotification(user_id, data.amount, data.auto_date);
   return { automatized: true };
@@ -29,6 +32,7 @@ const createIncomeAutomatization = async (data, user_id) => {
     user_id
   };
   cron.schedule(`0 7 ${data.auto_date} * *`, async () => {
+    sendIncomesNotification(user_id, data.amount, data.name);
     await incomes.create(incomeObj);
   }, { jobName: data.name, user_id });
   await sendIncomesAutoNotification(user_id, data.amount, data.auto_date);
