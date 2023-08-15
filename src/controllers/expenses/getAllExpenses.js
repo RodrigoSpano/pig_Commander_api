6e0e6197f28690ea20c1d997a9f0fe6a0ecae98b
Benjamin Padilla <benja.padilla@outlook.com.ar>
2023-08-15 09:51:17 -0300

    fix(api/autodate): removing

--- src/controllers/expenses/getAllExpenses.js
+++ src/controllers/expenses/getAllExpenses.js
@@ -4,7 +4,9 @@ const { getTokenPayload } = require('../../utils/helpers/authHelpers');
 const getAllExpenses = async (req, res) => {
   try {
     const { id } = getTokenPayload(req.headers['authorization']);
-    const allExepenses = await expenses.findAll({ where: { user_id: id, automatized: false } });
+    const allExepenses = await expenses.findAll({
+      where: { user_id: id },
+    });
     return res.status(200).json(allExepenses);
   } catch (error) {
     return res.status(500).json({ error: error.message });
--- src/controllers/expenses/postExpenses.js
+++ src/controllers/expenses/postExpenses.js
@@ -1,16 +1,16 @@
 const { expenses } = require('../../db');
 const { getTokenPayload } = require('../../utils/helpers/authHelpers');
-const { sendExpensesNotification } = require('../../utils/helpers/sendMailHelper');
+const {
+  sendExpensesNotification,
+} = require('../../utils/helpers/sendMailHelper');
 
 const postExpenses = async (req, res) => {
   try {
     const { id: user_id } = getTokenPayload(req.headers['authorization']);
-    const { category_id, method_id, amount, automatized, auto_date, name } = req.body;
+    const { category_id, method_id, amount, name } = req.body;
     const newExpense = await expenses.create({
       name,
       amount,
-      automatized: automatized || false,
-      auto_date: automatized ? auto_date : null,
       method_id,
       category_id,
       user_id,
