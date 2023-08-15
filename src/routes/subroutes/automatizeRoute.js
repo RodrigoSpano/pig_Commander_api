a4821bfdeb65fcd9339822fa3dd955daeb3d02c8
RodrigoSpano <rorrospano17@gmail.com>
2023-08-15 13:42:48 -0300

    feature(transactions/automatize): create, get and stop automatize transactions

--- src/routes/subroutes/automatizeRoute.js
+++ src/routes/subroutes/automatizeRoute.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { createAutoTransaction } = require('../../controllers/automatization/createAutomatization');
+const { createAutoTransaction } = require('../../controllers/automatization/createAutomated');
 const stopAutomatize = require('../../controllers/automatization/stopAutomated');
 const automatedExistsMiddleware = require('../../utils/middlewares/automatedMiddleware');
+const getAllAutomatedTransactions = require('../../controllers/automatization/getAllAutomated');
 
 const router = Router();
 
+router.get('/transactions', getAllAutomatedTransactions);
 router.post('/transaction', automatedExistsMiddleware, createAutoTransaction);
 router.delete('/stop/:jobName', stopAutomatize);
 
