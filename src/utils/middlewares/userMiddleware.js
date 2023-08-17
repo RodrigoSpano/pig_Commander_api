fe26ed581977183cfeac3c54e10957053cda414a
RodrigoSpano <rorrospano17@gmail.com>
2023-08-17 11:35:16 -0300

    feature(user/banned): middleware for login and signup for banned users

---
+++ src/utils/middlewares/userMiddleware.js
@@ -0,0 +1,9 @@
+const { user } = require('../../db');
+
+const isBannedMiddleware = async (req, res, next) => {
+  const findUser = await user.findOne({ where: { email: req.body.email } });
+  if (findUser && findUser.deletedAt !== null) return res.status(401).json({ message: 'this email account has been banned' });
+  return next();
+};
+
+module.exports = { isBannedMiddleware };
