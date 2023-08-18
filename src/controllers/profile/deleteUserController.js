130bac4c55363d135188bec20ac953710ad8d3c3
RodrigoSpano <rorrospano17@gmail.com>
2023-08-18 13:57:08 -0300

    feat(auth): github authentication strategy

--- src/controllers/profile/deleteUserController.js
+++ src/controllers/profile/deleteUserController.js
@@ -3,7 +3,7 @@ const { user } = require('../../db');
 const deleteUser = async (req, res) => {
   try {
     const { id } = req.params;
-    await user.destroy({ where: { id } });
+    await user.destroy({ where: { id }, force: true });
     return res.status(200).json({ success: true });
   } catch (error) {
     return res.status(500).json({ error: error.message });
